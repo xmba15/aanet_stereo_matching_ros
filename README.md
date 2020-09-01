c2e94679de28411f57337da1130c78632a446401
xmba15 <thba1590@gmail.com>
2020-09-01 18:30:14 +0900

    point cloud projection

--- README.md
+++ README.md
@@ -1,30 +1,55 @@
 # 📝 aanet_stereo_matching_ros #
 ***
 
+ROS package for stereo matching and point cloud projection using [AANet](https://arxiv.org/abs/2004.09548)
+
 ## :tada: TODO
 ***
 
-- [x] a
-- [ ] b
+- [x] create ros nodes to get disparity map from stereo matching with AANet
+- [x] create pseudo-lidar point cloud from disparity map with camera info
 
 ## 🎛  Dependencies
 ***
-- [AANet](https://github.com/haofeixu/aanet)
-```bash
-    git submodule update --init --recursive
-```
 
-- conda environment
+- create stereo_matching conda environment
 ```bash
     conda env create --file environment.yml
 ```
 
+- Fetch [AANet](https://github.com/haofeixu/aanet)
+```bash
+    git submodule update --init --recursive
+```
+
+- Activate conda environment and build deformable convolution for aanet
+```bash
+    conda activate stereo_matching
+    cd ./scripts/aanet/nets/deform_conv && bash build.sh
+```
+
+- python3: this package (also the original implementation) uses python3, which is taboo for ROS1. One solution to avoid confliction between ROS1 and python3 is to run roslaunch under the directory with stereo_matching env activated.
+
 ## 🔨 How to Build ##
 ***
-YYYYnprocYYnprocYYY
+Better to build with catkin or colcon
 
 ## :running: How to Run ##
 ***
+
+- A sample rosbag of stereo images created from [DrivingStereo Dataset](https://drivingstereo-dataset.github.io/) with [this tool](https://github.com/xmba15/image_dataset_to_rosbag) has been prepared [HERE](https://drive.google.com/file/d/1MtEHVBYFhwIODbZDrk7tJ5AdOeW9r3UE/view?usp=sharing). Please download and extract into ./data/output.bag
+
+- Download AANet+ KITTI 2015 model from [MODEL ZOO](https://github.com/haofeixu/aanet/blob/master/MODEL_ZOO.md) and put into ./data/aanet+_kitti15-2075aea1.pth
+
+- Start sample roslaunch
+```bash
+    roslaunch aanet_stereo_matching_ros aanet_stereo_matching_ros_rviz_sample.launch
+```
+
+Please take a look at the sample roslaunch to know how to use the parameters.
+Tune img_scale with bigger value (1.0 means original image size) would yeild better point cloud at the cost of speed. Currently the perspective projection from disparity to point cloud in python seems costly. Might be better to just propagate disparity message and do the projection in C++ package.
+
+![aanet_sample](./docs/images/aanet_sample.png)
 
 ## :gem: References ##
 ***
--- scripts/stereo_matching_node.py
+++ scripts/stereo_matching_node.py
@@ -3,15 +3,16 @@ from __future__ import absolute_import, division, print_function
 import enum
 import os
 import sys
-
-import rospy
-from aanet_stereo_matching import AANetStereoMatcher, AANetStereoMatcherConfig
-from ros_node_base import RosNodeBase
-
-from sensor_msgs.msg import Image
-
-
+import numpy as np
 import torch
+import cv2
+
+from aanet_stereo_matching import AANetStereoMatcher, AANetStereoMatcherConfig
+from utility import Utility as utils
+from ros_node_base import RosNodeBase
+import rospy
+import message_filters
+from sensor_msgs.msg import Image, CameraInfo, PointCloud2
 
 
 class MatcherType(enum.IntEnum):
@@ -23,9 +24,9 @@ _MODEL_FACTORY = {MatcherType.AANET: [AANetStereoMatcher, AANetStereoMatcherConf
 
 
 class StereoMatcherNode(RosNodeBase):
-    def __init__(self, rospy):
-        RosNodeBase.__init__(self, rospy)
-        self._matcher_type = self._rospy.get_param("~matcher_type", 0)
+    def __init__(self, internal_rospy):
+        RosNodeBase.__init__(self, internal_rospy)
+        self._init_parameter()
 
         if self._matcher_type > MatcherType.MAX:
             self._rospy.logfatal("Not supported stereo matcher\n")
@@ -38,7 +39,6 @@ class StereoMatcherNode(RosNodeBase):
             self._rospy.logfatal("Invalid model path {}\n".format(self._model_config.model_path))
             sys.exit()
 
-        self._gpu_idx = self._rospy.get_param("~gpu_idx", -1)
         if self._gpu_idx < 0:
             self._device = torch.device("cpu")
         else:
@@ -50,52 +50,95 @@ class StereoMatcherNode(RosNodeBase):
 
         self._model = self._MODEL_CLASS(self._model_config, self._device)
 
-        self._debug = rospy.get_param("~debug", True)
-
         self._disp_pub = self._rospy.Publisher("~disparity", Image, queue_size=1)
         if self._debug:
             self._disp_debug_pub = self._rospy.Publisher("~disp_debug", Image, queue_size=1)
 
-        self._disparity_multiplier = self._rospy.get_param("~disparity_multiplier", 256.0)
+        if self._publish_point_cloud:
+            self._pointcloud_pub = self._rospy.Publisher("~pointcloud", PointCloud2, queue_size=1)
 
+        self._subscribe_once()
         self._subscribe()
 
+    def _init_parameter(self):
+        self._matcher_type = self._rospy.get_param("~matcher_type", 0)
+        self._gpu_idx = self._rospy.get_param("~gpu_idx", -1)
+        self._debug = self._rospy.get_param("~debug", True)
+        self._img_scale = self._rospy.get_param("~img_scale", 1.0)
+        self._disparity_multiplier = self._rospy.get_param("~disparity_multiplier", 256.0)
+        self._max_depth = self._rospy.get_param("~max_depth", 30.0)
+        self._publish_point_cloud = self._rospy.get_param("~publish_point_cloud", False)
+        self._use_raw_img = self._rospy.get_param("~use_raw_img", False)
+
     def _subscribe(self):
-        import message_filters
 
         self._synchronizer_type = self._rospy.get_param("~synchronizer_type", 0)
-
         self._left_rect_img_sub = message_filters.Subscriber("~left_rect_img", Image)
         self._right_rect_img_sub = message_filters.Subscriber("~right_rect_img", Image)
 
         self._ts = self.to_synchronizer(
-            self._synchronizer_type, fs=[self._left_rect_img_sub, self._right_rect_img_sub], queue_size=10, slop=0.1
+            self._synchronizer_type,
+            fs=[
+                self._left_rect_img_sub,
+                self._right_rect_img_sub,
+            ],
+            queue_size=10,
+            slop=0.1,
         )
         self._ts.registerCallback(self.callback)
 
-    def callback(self, left_rect_img_msg, right_rect_img_msg):
-        import cv2
-        import numpy as np
+    def _subscribe_once(self):
+        self._left_camera_info = self._rospy.wait_for_message("~left_camera_info", CameraInfo)
+        self._right_camera_info = self._rospy.wait_for_message("~right_camera_info", CameraInfo)
+        self._q_matrix = utils.get_q_matrix(self._left_camera_info, self._right_camera_info)
+        if not np.isclose(self._img_scale, 1.0):
+            scaled_left_camera_info = utils.scale_camera_info(self._left_camera_info, self._img_scale, self._img_scale)
+            scaled_right_camera_info = utils.scale_camera_info(
+                self._right_camera_info, self._img_scale, self._img_scale
+            )
+            self._q_matrix = utils.get_q_matrix(scaled_left_camera_info, scaled_right_camera_info)
 
-        left_img = RosNodeBase.to_cv_image(left_rect_img_msg)
-        right_img = RosNodeBase.to_cv_image(right_rect_img_msg)
-
+    def callback(self, left_img_msg: Image, right_img_msg: Image):
+        left_img = utils.to_cv_image(left_img_msg)
         if left_img is None:
             rospy.logwarn("Left image empty\n")
-            pass
+            return
+
+        right_img = utils.to_cv_image(right_img_msg)
         if right_img is None:
             rospy.logwarn("Right image empty\n")
-            pass
+            return
+
+        if self._use_raw_img:
+            left_img = utils.remap(left_img, self._left_camera_info)
+            right_img = utils.remap(right_img, self._right_camera_info)
+
+        if not np.isclose(self._img_scale, 1.0):
+            width = int(left_img.shape[1] * self._img_scale)
+            height = int(left_img.shape[0] * self._img_scale)
+            resize_dim = (width, height)
+            left_img = cv2.resize(left_img, resize_dim, interpolation=cv2.INTER_CUBIC)
+            right_img = cv2.resize(right_img, resize_dim, interpolation=cv2.INTER_CUBIC)
 
         disp_img = self._model.run(left_img, right_img)
-        disp_img = disp_img * self._disparity_multiplier
-        disp_img = disp_img.astype(np.uint16)
 
-        self._disp_pub.publish(RosNodeBase.to_img_msg(disp_img, "16UC1"))
+        cur_header = self.get_new_header(left_img_msg.header)
+
+        disp_img_msg = utils.to_img_msg(disp_img, "32FC1", cur_header)
+        self._disp_pub.publish(disp_img_msg)
 
         if self._debug:
-            disp_debug_img = cv2.applyColorMap(RosNodeBase.uint16_to_uint8(disp_img), cv2.COLORMAP_JET)
-            self._disp_debug_pub.publish(RosNodeBase.to_img_msg(disp_debug_img, "8UC3"))
+            disp_img_scaled = disp_img * self._disparity_multiplier
+            disp_img_scaled = disp_img_scaled.astype(np.uint16)
+
+            disp_debug_img = cv2.applyColorMap(utils.uint16_to_uint8(disp_img_scaled), cv2.COLORMAP_JET)
+            disp_debug_img_msg = utils.to_img_msg(disp_debug_img, "bgr8", cur_header)
+            self._disp_debug_pub.publish(disp_debug_img_msg)
+
+        if self._publish_point_cloud:
+            projected_points = cv2.reprojectImageTo3D(disp_img, self._q_matrix)
+            pointcloud_msg = utils.xyzrgb_array_to_pointcloud2(projected_points, left_img, self._max_depth, cur_header)
+            self._pointcloud_pub.publish(pointcloud_msg)
 
 
 def main(argv):
